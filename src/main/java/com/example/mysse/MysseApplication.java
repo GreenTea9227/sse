4c08b7efc740de01a6ff02591e7d9440bf7695f8
yohan <sjohn9227@naver.com>
2023-05-15 15:56:22 +0900

    sse 기본 뼈대

---
+++ src/main/java/com/example/mysse/MysseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysseApplication.class, args);
+    }
+
+}
